5781bf3d8d10f6056bf319f0287b5d0390c22107
ALİHAN UZUN <alihanuzun4@gmail.com>
2024-09-10 13:37:57 +0300

    Alarm

---
+++ Alarm/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAlarmKur = new System.Windows.Forms.Button();
+            this.btnAlarmErteleme = new System.Windows.Forms.Button();
+            this.btnAlarmDurdur = new System.Windows.Forms.Button();
+            this.lblGuncelZaman = new System.Windows.Forms.Label();
+            this.txtAlarmZamani = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnAlarmKur
+            // 
+            this.btnAlarmKur.BackColor = System.Drawing.Color.Red;
+            this.btnAlarmKur.FlatAppearance.BorderSize = 0;
+            this.btnAlarmKur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlarmKur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAlarmKur.ForeColor = System.Drawing.Color.White;
+            this.btnAlarmKur.Location = new System.Drawing.Point(165, 245);
+            this.btnAlarmKur.Name = "btnAlarmKur";
+            this.btnAlarmKur.Size = new System.Drawing.Size(143, 56);
+            this.btnAlarmKur.TabIndex = 0;
+            this.btnAlarmKur.Text = "Kur";
+            this.btnAlarmKur.UseVisualStyleBackColor = false;
+            this.btnAlarmKur.Click += new System.EventHandler(this.btnAlarmKur_Click);
+            // 
+            // btnAlarmErteleme
+            // 
+            this.btnAlarmErteleme.BackColor = System.Drawing.Color.Red;
+            this.btnAlarmErteleme.FlatAppearance.BorderSize = 0;
+            this.btnAlarmErteleme.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlarmErteleme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAlarmErteleme.ForeColor = System.Drawing.Color.White;
+            this.btnAlarmErteleme.Location = new System.Drawing.Point(314, 245);
+            this.btnAlarmErteleme.Name = "btnAlarmErteleme";
+            this.btnAlarmErteleme.Size = new System.Drawing.Size(143, 56);
+            this.btnAlarmErteleme.TabIndex = 1;
+            this.btnAlarmErteleme.Text = "Ertele";
+            this.btnAlarmErteleme.UseVisualStyleBackColor = false;
+            this.btnAlarmErteleme.Click += new System.EventHandler(this.btnAlarmErteleme_Click);
+            // 
+            // btnAlarmDurdur
+            // 
+            this.btnAlarmDurdur.BackColor = System.Drawing.Color.Red;
+            this.btnAlarmDurdur.FlatAppearance.BorderSize = 0;
+            this.btnAlarmDurdur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlarmDurdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAlarmDurdur.ForeColor = System.Drawing.Color.White;
+            this.btnAlarmDurdur.Location = new System.Drawing.Point(463, 245);
+            this.btnAlarmDurdur.Name = "btnAlarmDurdur";
+            this.btnAlarmDurdur.Size = new System.Drawing.Size(143, 56);
+            this.btnAlarmDurdur.TabIndex = 2;
+            this.btnAlarmDurdur.Text = "Durdur";
+            this.btnAlarmDurdur.UseVisualStyleBackColor = false;
+            this.btnAlarmDurdur.Click += new System.EventHandler(this.btnAlarmDurdur_Click);
+            // 
+            // lblGuncelZaman
+            // 
+            this.lblGuncelZaman.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblGuncelZaman.Location = new System.Drawing.Point(170, 71);
+            this.lblGuncelZaman.Name = "lblGuncelZaman";
+            this.lblGuncelZaman.Size = new System.Drawing.Size(426, 70);
+            this.lblGuncelZaman.TabIndex = 3;
+            this.lblGuncelZaman.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAlarmZamani
+            // 
+            this.txtAlarmZamani.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAlarmZamani.Location = new System.Drawing.Point(241, 165);
+            this.txtAlarmZamani.Name = "txtAlarmZamani";
+            this.txtAlarmZamani.Size = new System.Drawing.Size(281, 38);
+            this.txtAlarmZamani.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(173, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(438, 80);
+            this.label2.TabIndex = 5;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAlarmZamani);
+            this.Controls.Add(this.lblGuncelZaman);
+            this.Controls.Add(this.btnAlarmDurdur);
+            this.Controls.Add(this.btnAlarmErteleme);
+            this.Controls.Add(this.btnAlarmKur);
+            this.Name = "Form1";
+            this.Text = "Alarm Projesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAlarmKur;
+        private System.Windows.Forms.Button btnAlarmErteleme;
+        private System.Windows.Forms.Button btnAlarmDurdur;
+        private System.Windows.Forms.Label lblGuncelZaman;
+        private System.Windows.Forms.TextBox txtAlarmZamani;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Alarm/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Alarm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SoundPlayer ses = new SoundPlayer("ses.wav");
+        int ertelemesuresi = 0;
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblGuncelZaman.Text = DateTime.Now.ToLongTimeString();
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblGuncelZaman.Text = DateTime.Now.ToLongTimeString();
+            if (lblGuncelZaman.Text==txtAlarmZamani.Text)
+            {
+                ses.Play();
+            }
+        }
+
+        private void btnAlarmKur_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime zaman = Convert.ToDateTime(txtAlarmZamani.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Geçerli Zaman Giriniz");
+                txtAlarmZamani.Focus();
+                txtAlarmZamani.SelectAll();
+                return;
+            }
+        }
+
+        private void btnAlarmErteleme_Click(object sender, EventArgs e)
+        {
+            ertelemesuresi = 5;
+            timer2.Enabled = true;
+            label2.Text = ertelemesuresi.ToString() + " Saniye Sonra Alarm Çalacak";
+            ses.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            while (ertelemesuresi==0)
+            {
+                ses.Play();
+                timer1.Enabled = true;
+                timer2.Enabled = false;
+                label2.Text = "Kalkma Vakti!!!";
+                return;
+            }
+            ertelemesuresi--;
+            label2.Text = ertelemesuresi.ToString() + " Saniye Sonra Alarm Çalacak";
+        }
+
+        private void btnAlarmDurdur_Click(object sender, EventArgs e)
+        {
+            ses.Stop();
+            timer2.Stop();
+            label2.Text = "";
+        }
+    }
+}
